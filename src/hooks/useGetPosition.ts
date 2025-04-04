39b26929b6c9e455619a25c19c90a550691cb402
liara987 <987.liara@gmail.com>
2025-04-04 12:33:00 -0300

    feat: hook to get positions

---
+++ src/hooks/useGetPosition.ts
@@ -0,0 +1,42 @@
+import { useContext, useEffect, useState } from "react";
+import { EquipmentPositionHistoryContext } from "../context/EquipmentContext";
+
+interface PositionData {
+  equipmentId: string;
+  date: string;
+  lat: number;
+  lon: number;
+}
+
+function useGetPosition() {
+  const [positionData, setPositionData] = useState<PositionData[]>([]);
+  const equipmentPosition = useContext(EquipmentPositionHistoryContext);
+
+  useEffect(() => {
+    try {
+      if (!equipmentPosition || !Array.isArray(equipmentPosition)) {
+        console.error(
+          "Erro: Contexto EquipmentPositionHistoryContext é inválido ou indefinido.",
+        );
+        return;
+      }
+
+      const positions: PositionData[] = equipmentPosition.flatMap((equipment) =>
+        equipment.positions.map((posi) => ({
+          equipmentId: equipment.equipmentId,
+          date: posi.date,
+          lat: posi.lat,
+          lon: posi.lon,
+        })),
+      );
+
+      setPositionData(positions);
+    } catch (error) {
+      console.error("Erro ao processar as posições do equipamento: ", error);
+    }
+  }, [equipmentPosition]);
+
+  return positionData;
+}
+
+export default useGetPosition;
